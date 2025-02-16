295cc2a93c9902394cac5b984aa6319b564d89d0
Hasan Moosavi <h.mousavi910@gmail.com>
2025-02-16 23:11:05 +0330

    Connected config to DB

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,3 +1,5 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfig } from 'src/config/typeorm.config';
 import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { join } from 'path';
@@ -6,7 +8,7 @@ import { join } from 'path';
   imports: [ConfigModule.forRoot({
     isGlobal: true,
     envFilePath: join(process.cwd(), ".env")
-  })],
+  }), TypeOrmModule.forRoot(TypeOrmConfig())],
   controllers: [],
   providers: [],
 })
