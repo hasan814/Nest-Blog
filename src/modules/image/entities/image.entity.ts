223847a727da4d79aae09a8991c1ca9549f51322
Hasan Moosavi <h.mousavi910@gmail.com>
2025-03-20 21:24:29 +0330

    Finished Image CRUD

--- src/modules/image/entities/image.entity.ts
+++ src/modules/image/entities/image.entity.ts
@@ -1,4 +1,4 @@
-import { Column, CreateDateColumn, Entity, ManyToOne } from "typeorm";
+import { AfterLoad, Column, CreateDateColumn, Entity, ManyToOne } from "typeorm";
 import { BaseEntity } from "src/common/abstracts/base.entity";
 import { UserEntity } from "src/modules/user/entities/user.entity";
 import { EntityName } from "src/common/enums/entity.enums";
@@ -22,4 +22,9 @@ export class ImageEntity extends BaseEntity {
 
   @ManyToOne(() => UserEntity, user => user.images, { onDelete: "CASCADE" })
   user: UserEntity
+
+  @AfterLoad()
+  map() {
+    this.location = `http://localhost:3000/${this.location}`
+  }
 }
--- src/modules/image/image.service.ts
+++ src/modules/image/image.service.ts
@@ -1,27 +1,51 @@
+import { Inject, Injectable, NotFoundException, Scope } from '@nestjs/common';
+import { NotFoundMessage, PublicMessage } from 'src/common/enums/message.enum';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ImageEntity } from './entities/image.entity';
-import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { MulterFile } from 'src/common/utils/multer.util';
 import { ImageDto } from './dto/image.dto';
+import { Request } from 'express';
+import { REQUEST } from '@nestjs/core';
 
-@Injectable()
+@Injectable({ scope: Scope.REQUEST })
 export class ImageService {
-  constructor(@InjectRepository(ImageEntity) private imageRepository: Repository<ImageEntity>) { }
+  constructor(
+    @InjectRepository(ImageEntity) private imageRepository: Repository<ImageEntity>,
+    @Inject(REQUEST) private req: Request
+  ) { }
 
-  create(imageDto: ImageDto, image: MulterFile) {
-    return image;
+  async create(imageDto: ImageDto, image: MulterFile) {
+    const userId = this.req?.user?.id
+    const { alt, name } = imageDto
+    let location = image?.path?.slice(7)
+    await this.imageRepository.insert({
+      alt: alt || name,
+      name,
+      location,
+      userId
+    })
+    return { message: PublicMessage.Created };
   }
 
   findAll() {
-    return `This action returns all image`;
+    const userId = this.req?.user?.id
+    return this.imageRepository.find({ where: { userId }, order: { id: "DESC" } });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} image`;
+  async findOne(id: number) {
+    const userId = this.req?.user?.id
+    const image = await this.imageRepository.find({
+      where: { userId, id },
+      order: { id: "DESC" }
+    });
+    if (!image) throw new NotFoundException(NotFoundMessage.NotFoundPost)
+    return image
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} image`;
+  async remove(id: number) {
+    const image = await this.findOne(id)
+    await this.imageRepository.remove(image)
+    return { message: PublicMessage.Deleted }
   }
 }
