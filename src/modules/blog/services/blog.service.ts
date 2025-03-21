1639f97cef2e4e201ddd9e109b7faf73abd34072
Hasan Moosavi <h.mousavi910@gmail.com>
2025-03-21 12:01:05 +0330

    Add SuggestionBlogs to get Random Blogs

--- src/modules/blog/services/blog.service.ts
+++ src/modules/blog/services/blog.service.ts
@@ -2,6 +2,7 @@ import { BadRequestException, forwardRef, Inject, Injectable, NotFoundException,
 import { BadRequestMessage, NotFoundMessage, PublicMessage } from 'src/common/enums/message.enum';
 import { CreateBlogDto, FilterBlogDto, UpdateBlogDto } from '../dto/blog.dto';
 import { paginationGenerator, paginationSolver } from 'src/common/utils/pagination.utils';
+import { DataSource, Repository } from 'typeorm';
 import { createSlug, randomId } from 'src/common/utils/slug.util';
 import { BlogCommentService } from './comment.service';
 import { BlogCategoryEntity } from '../entities/blog-category.entity';
@@ -11,7 +12,6 @@ import { CategoryService } from '../../category/category.service';
 import { BlogLikesEntity } from '../entities/like.entity';
 import { BlogBookEntity } from '../entities/bookmark.entity';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
-import { Repository } from 'typeorm';
 import { BlogStatus } from '../enum/status.enum';
 import { BlogEntity } from '../entities/blog.entity';
 import { EntityName } from 'src/common/enums/entity.enums';
@@ -30,6 +30,7 @@ export class BlogService {
     @InjectRepository(BlogCategoryEntity) private blogCategoryRepository: Repository<BlogCategoryEntity>,
     @Inject(REQUEST) private request: RequestWithUser,
     private categoryService: CategoryService,
+    private dataSource: DataSource
   ) { }
 
   async create(blogDto: CreateBlogDto) {
@@ -181,7 +182,7 @@ export class BlogService {
   }
 
   async findOneBySlug(slug: string, paginationDto: PaginationDto) {
-    const userId = this.request?.user?.id
+    const userId = this.request?.user?.id;
     const blog = await this.blogRepository.createQueryBuilder(EntityName.Blog)
       .leftJoin("blog.categories", "categories")
       .leftJoin("categories.category", "category")
@@ -191,16 +192,63 @@ export class BlogService {
       .where({ slug })
       .loadRelationCountAndMap('blog.likes', 'blog.likes')
       .loadRelationCountAndMap('blog.bookmarks', 'blog.bookmarks')
-      .getOne()
-    if (!blog) throw new NotFoundException(NotFoundMessage.NotFoundPost)
-    const commentsData = await this.blogCommentService.findCommentsOfBlog(blog.id, paginationDto)
+      .getOne();
+
+    if (!blog) throw new NotFoundException(NotFoundMessage.NotFoundPost);
+
+    const commentsData = await this.blogCommentService.findCommentsOfBlog(blog.id, paginationDto);
+
     let isLiked = false;
-    let isBookmarked = false
+    let isBookmarked = false;
     if (userId && !isNaN(userId) && userId > 0) {
-      isLiked = !!(await this.blogLikeRepository.findOneBy({ userId, blogId: blog.id }))
-      isBookmarked = !!(await this.blogBookmarkRepository.findOneBy({ userId, blogId: blog.id }))
+      isLiked = !!(await this.blogLikeRepository.findOneBy({ userId, blogId: blog.id }));
+      isBookmarked = !!(await this.blogBookmarkRepository.findOneBy({ userId, blogId: blog.id }));
     }
-    return { blog, isLiked, isBookmarked, commentsData }
+
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+
+    const suggestBlogs = await queryRunner.query(`
+      WITH suggested_blogs AS (
+        SELECT
+          blog.id,
+          blog.slug,
+          blog.image,
+          blog.title,
+          blog.description,
+          blog.time_for_study,
+          json_build_object(
+            'username', u.username,
+            'author_name', p.nick_name,
+            'image', p.image_profile
+          ) AS author,
+          array_agg(DISTINCT cat.title) AS categories,
+          (
+            SELECT COUNT(*) FROM blog_likes
+            WHERE blog_likes."blogId" = blog.id
+          ) AS likes,
+          (
+            SELECT COUNT(*) FROM blog_bookmarks
+            WHERE blog_bookmarks."blogId" = blog.id
+          ) AS bookmarks,
+          (
+            SELECT COUNT(*) FROM blog_comments
+            WHERE blog_comments."blogId" = blog.id
+          ) AS comments
+        FROM blog
+        LEFT JOIN public.user u ON blog."authorId" = u.id  
+        LEFT JOIN profile p ON p."userId" = u.id  
+        LEFT JOIN blog_category bc ON blog.id = bc."blogId"  
+        LEFT JOIN category cat ON bc."categoryId" = cat.id  
+        GROUP BY blog.id, u.username, p.nick_name, p.image_profile
+        ORDER BY RANDOM()
+        LIMIT 3
+      )
+      SELECT * FROM suggested_blogs
+    `);
+
+    return { blog, isLiked, isBookmarked, commentsData, suggestBlogs };
   }
+
 
 }
